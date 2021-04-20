7c26a3f1bab2ead771fc76dd6fbd14ac3ab514c1
jmunozz <jmunoz@student.42.fr>
2021-04-20 17:57:18 +0200

    ADD: atc table

--- create_database/main.py
+++ create_database/main.py
@@ -1,4 +1,6 @@
 from os import path
+from pathlib import Path
+import json
 
 import pandas as pd
 
@@ -26,8 +28,50 @@ def load_to_df_bdpm_cis(path, settings):
     args = {**{ "filepath_or_buffer": path}, **settings}
     df = pd.read_csv(**args)
     # Put substance_active field in lower case
-    helpers.serie_to_lowercase(df, settings["names"])
+    helpers.serie_to_lowercase(df, settings["names"][1:])
     return df
 
 
+def create_table_rsp_compo(settings):
+    fpath = download_rsp_compo()
+    df = load_to_df_rsp_compo(fpath, settings["read_csv"])
+    db.create_table_from_df(df, settings["to_sql"])
+
+
+def download_rsp_compo():
+    url = settings.RSP_COMPO_URL
+    tmp_folder = settings.TMP_FOLDER
+    fpath = path.join(tmp_folder, 'RSP_COMPO.txt')
+    helpers.download_file_from_url(url, fpath)
+    return fpath
+
+
+def load_to_df_rsp_compo(fpath, settings):
+    args = {**{ "filepath_or_buffer": fpath}, **settings}
+    df = pd.read_csv(**args)
+    # Cleaning
+    df = df[df.nature_composant == "SA"]
+    df = df.rename(columns={"substance_active": "nom"})
+    df = df[["nom"]]
+    df = df[~df.index.duplicated(keep="first")]
+    helpers.serie_to_lowercase(df, ["nom"])
+    return df
+
+def create_table_atc(settings):
+    fpath = find_atc(settings["source"]["pattern"])
+    if fpath.exists():
+        df = load_to_df_atc(fpath)
+        db.create_table_from_df(df, settings["to_sql"])
+
+def find_atc(pattern): 
+    return helpers.list_files(settings.DATA_FOLDER, pattern)[0]
+
+def load_to_df_atc(fpath): 
+    serie = pd.read_json(fpath, typ="series")
+    df = serie.to_frame("label_atc")
+    df.index.set_names(names="code_atc", inplace=True)
+    return df
+
 create_table_bdpm_cis(settings.files["bdpm_cis"])
+create_table_rsp_compo(settings.files["rsp_compo"])
+create_table_atc(settings.files["atc"])
--- create_database/settings.py
+++ create_database/settings.py
@@ -1,15 +1,18 @@
 from decouple import config
-from sqlalchemy.types import Integer, Text, Date
+from sqlalchemy.sql.sqltypes import VARCHAR
+from sqlalchemy.sql.type_api import INDEXABLE
+from sqlalchemy.types import Integer, Text, Date, String
 
 
 ENV = config("ENV", default="staging")
 BDPM_CIS_URL = config("BDPM_CIS_URL", "https://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=CIS_bdpm.txt")
+RSP_COMPO_URL = config("RSP_COMPO_URL", "http://agence-prd.ansm.sante.fr/php/ecodex/telecharger/lirecomp.php")
 TMP_FOLDER = config("TMP_FOLDER", "/tmp")
+DATA_FOLDER = config("DATA_FOLDER", "./data")
 DBHOSTNAME= config("DBHOSTNAME")
 DBUSERNAME= config("DBUSERNAME")
 DBPWD= config("DBPWD")
 DBNAME= config("DBNAME")
-
 
 
 files = {
@@ -32,15 +35,16 @@ files = {
                 "surveillance_renforcee"
             ],
             "header": None,
+            "index_col": "cis",
             "parse_dates":["date_amm"],
             "dtype":{"cis": str},    
         },
         "to_sql": {
             "name": "specialite",
             "if_exists": "replace",
-            "index": "False",
+            "index": True,
             "dtype": {
-                "cis": Text,
+                "cis": String(16),
                 "nom": Text,
                 "forme_pharma": Text,
                 "voie_admin": Text,
@@ -53,6 +57,46 @@ files = {
                 "titulaires": Text,
                 "surveillance_renforcee": Text,
             }
+        }
+    },
+    "rsp_compo": {
+        "read_csv": {
+            "sep":"\t",
+            "encoding":"latin1",
+            "names":[
+                "cis",
+                "elem_pharma",
+                "code",
+                "substance_active",
+                "dosage",
+                "ref_dosage",
+                "nature_composant",
+                "num_lien",
+                "v",
+            ],
+            "header": None,
+            "index_col": 2,
+            "dtype":{"cis": str, "code": str},    
+        },
+        "to_sql": {
+            "name": "substance",
+            "if_exists": "replace",
+            "index": True,
+            "dtype": {
+                "code": String(16),
+                "nom": Text,
+            }
+        }
+    },
+    "atc": {
+        "source": {
+            "pattern": "atc_names.json"
+        },
+        "to_sql": {
+            "name": "classes_atc",
+            "if_exists": "replace",
+            "index": True,
+            "dtype": {"code_atc": String(16)}
         }
     }
 }
