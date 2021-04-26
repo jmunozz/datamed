b07adf224e7ff1c32765ba6569bac82d70a5c5c1
jmunozz <jmunoz@student.42.fr>
2021-04-26 13:07:48 +0200

    FIX: small fixes and add filter for RSSI

--- create_database/main.py
+++ create_database/main.py
@@ -101,9 +101,11 @@ def create_spe_patients_age_table(_settings):
 def create_substance_ordei_table(_settings):
     df = helpers.load_csv_to_df(_settings[0])
     df_by_years = df.groupby(["code", "annee"]).agg(conso_annee=("conso", "sum"), cas_annee=("cas", "sum"))
-    df_by_years["exposition"] = df_by_years["conso_annee"].apply(helpers.get_exposition_level, type="substance")
-    df_by_code = df_by_years.groupby("code").agg(conso=("conso_annee", "sum"), cas=("cas_annee", "sum"))
+    df_by_years["exposition_annee"] = df_by_years["conso_annee"].apply(helpers.get_exposition_level, type="substance")
+    df_by_code = df_by_years.groupby("code").agg(conso=("conso_annee", "sum"), cas=("cas_annee", "sum"), taux_exposition=("conso_annee", helpers.get_total_exposition_level))
     final_df = df_by_years.join(df_by_code, on=["code"])
+    final_df = helpers.filter_low_values(final_df, ["cas", "cas_annee"])
+    final_df["taux_cas"] = final_df.apply(axis=1, func=lambda x: x.cas * 100000 / x.conso if x.cas >= 10 else None)
     final_df.reset_index(inplace=True, level=["annee"])
     db.create_table_from_df(final_df, _settings[0]["to_sql"])
 
@@ -119,6 +121,7 @@ def create_substance_patients_sexe_table(_settings):
     )
     final_df = pd.merge(conso, conso_pct, on=["code", "sexe"])
     final_df.reset_index(inplace=True, level=["sexe"])
+    final_df.drop(["conso"], inplace=True, axis=1)
     db.create_table_from_df(final_df, _settings[1]["to_sql"])
 
 def create_substance_patients_age_table(_settings):
@@ -131,30 +134,35 @@ def create_substance_patients_age_table(_settings):
     )
     final_df = pd.merge(conso, conso_pct, on=["code", "age"])
     final_df.reset_index(inplace=True, level=["age"])
+    final_df.drop(["conso"], inplace=True, axis=1)
     db.create_table_from_df(final_df, _settings[2]["to_sql"])
 
 def create_substance_cas_sexe_table(_settings):
     df = helpers.load_csv_to_df(_settings[0])
     df["sexe"] = df["sexe"].apply(lambda x: helpers.mapSexeToCode(x))
     cas = df.groupby(["code", "sexe"])["cas"].sum().rename("cas")
+    cas = helpers.filter_serie_on_low_values(cas)
     cas_pct = (
         cas.groupby(level=0)
-        .apply(lambda x: 100 * x / float(x.sum()))
+        .apply(lambda x: 100 * x / float(x.sum()) if cas is not None else None)
         .rename("pourcentage_cas")
     )
     final_df = pd.merge(cas, cas_pct, on=["code", "sexe"])
+    final_df.drop(["cas"], axis=1, inplace=True)
     final_df.reset_index(inplace=True, level=["sexe"])
     db.create_table_from_df(final_df, _settings[3]["to_sql"])
 
 def create_substance_cas_age_table(_settings):
     df = helpers.load_csv_to_df(_settings[0])
     cas = df.groupby(["code", "age"])["cas"].sum().rename("cas")
+    cas = helpers.filter_serie_on_low_values(cas)
     cas_pct = (
         cas.groupby(level=0)
-        .apply(lambda x: 100 * x / float(x.sum()))
+        .apply(lambda x: 100 * x / float(x.sum()) if cas is not None else None)
         .rename("pourcentage_cas")
     )
     final_df = pd.merge(cas, cas_pct, on=["code", "age"])
+    final_df.drop(["cas"], axis=1, inplace=True)
     final_df.reset_index(inplace=True, level=["age"])
     db.create_table_from_df(final_df, _settings[4]["to_sql"])
 
@@ -176,24 +184,26 @@ def create_substance_soclong_table(_settings):
     total_case = total_case_per_sex_and_age.groupby("code").agg({ "n_cas": "sum" })
     decla_eff = df.groupby(["code", "soc_long"]).agg({ "n_decla_eff": "sum"}).reset_index(level="soc_long")
     final_df = pd.merge(total_case, decla_eff, left_index=True, right_on=["code"])
+    final_df = helpers.filter_df_on_low_values(final_df, ["n_decla_eff", "n_cas"])
     final_df["pourcentage_cas"] = final_df.apply(
-        lambda x: float(x.n_decla_eff * 100 / x.n_cas), axis=1, result_type="expand"
+        lambda x: float(x.n_decla_eff * 100 / x.n_cas) if x.n_decla_eff is not None and x.n_cas is not None else None, axis=1, result_type="expand"
     )
-    # final_df.drop(["n_cas"], inplace=True, axis=1)
+    final_df.drop(["n_cas"], inplace=True, axis=1)
     db.create_table_from_df(final_df, _settings["to_sql"])
 
 
 def create_hlt_table(_settings_soclong, _settings):
     df = helpers.load_csv_to_df(_settings)
-    soclong_df = helpers.load_csv_to_df(_settings_soclong)
-    total_case_per_sex_and_age = soclong_df.groupby(["code", "sexe", "age"]).agg({ "n_cas": "max"})
-    total_cases = total_case_per_sex_and_age.groupby(["code"]).agg({ "n_cas": "sum" })
+    soclong_df = helpers.load_csv_to_df(_settings_soclong) 
+    decla_eff = soclong_df.groupby(["code", "soc_long"]).agg({ "n_decla_eff": "sum"})
     hlt = df.groupby(["code", "soc_long", "effet_hlt"]).agg({"n_decla_eff_hlt": "sum"})
-    hlt.reset_index(["effet_hlt", "soc_long"], inplace=True)
-    final_df = pd.merge(total_cases, hlt, left_index=True, right_index=True)
+    hlt.reset_index(["effet_hlt"], inplace=True)
+    final_df = pd.merge(decla_eff, hlt, left_index=True, right_index=True)
     final_df["pourcentage_cas"] = final_df.apply(
-        lambda x: float(x.n_decla_eff_hlt * 100 / x.n_cas), axis=1, result_type="expand"
+        lambda x: float(x.n_decla_eff_hlt * 100 / x.n_decla_eff), axis=1, result_type="expand"
     )
+    final_df.reset_index(["soc_long"], inplace=True)
+    final_df.drop(["n_decla_eff_hlt", "n_decla_eff"], inplace=True, axis=1)
     db.create_table_from_df(final_df, _settings["to_sql"])
 
 
