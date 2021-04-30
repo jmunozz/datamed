8edf0ccf5ea3f8309ea97027a357a0d0792e27ea
jmunozz <jmunoz@student.42.fr>
2021-04-30 13:23:48 +0200

    FIX: add display NoData if df empty

--- datamed_dash/apps/components/specialite.py
+++ datamed_dash/apps/components/specialite.py
@@ -78,29 +78,19 @@ def get_notice_link(cis) -> str:
 
 
 def Specialite(cis: str) -> Component:
-    divs = [
-        Header(cis),
-        Description(cis),
-    ]
+
+    df_spe = specialite.get_specialite_df(cis)
+    df_sub = specialite.list_substances(cis)
+    df_age = specialite.get_age_df(cis)
+    df_sexe = specialite.get_sexe_df(cis)
     df_expo = specialite.get_exposition(cis)
-    if df_expo is not None:
-        df_age = specialite.get_age_df(cis)
-        df_sexe = specialite.get_sexe_df(cis)
-        divs.append(
-            PatientsTraites(
-                df_age=df_age,
-                df_sexe=df_sexe,
-                df_expo=df_expo,
-                index=cis,
-                pie_colors=PIE_COLORS_SPECIALITE,
-            )
-        )
-
-    df_emed = fetch_data.fetch_table("erreur_med_cis_denomination", "cis")
-    if cis in df_emed.index.tolist():
-        divs.append(ErreursMedicamenteuses(cis))
-
-    divs.append(EffetsIndesirables(cis))
+    df_desc = specialite.get_description_df(cis)
+    df_atc = specialite.get_atc_df(cis)
+    df_ei = specialite.get_erreur_med_effet_indesirable(cis)
+    df_cause = specialite.get_erreur_med_cause(cis)
+    df_nat = specialite.get_erreur_med_nature(cis)
+    df_pop = specialite.get_erreur_med_population(cis)
+    df_denom = specialite.get_erreur_med_denom(cis)
 
     return html.Div(
         [
@@ -117,8 +107,19 @@ def Specialite(cis: str) -> Component:
                 className="side-menu",
             ),
             html.Div(
-                html.Div(
-                    divs, className="container-fluid", style={"padding-left": "80px"}
+                html.Div([
+                    Header(df_spe),
+                    Description(df_spe, df_desc, df_atc, df_sub),
+                    PatientsTraites(
+                        df_age=df_age,
+                        df_sexe=df_sexe,
+                        df_expo=df_expo,
+                        index=cis,
+                        pie_colors=PIE_COLORS_SPECIALITE,
+                     ),
+                    ErreursMedicamenteuses(df_ei, df_pop, df_cause, df_nat, df_denom),
+                    EffetsIndesirables(df_sub),
+                ], className="container-fluid", style={"padding-left": "80px"}
                 ),
                 className="container-fluid side-content",
             ),
@@ -127,8 +128,7 @@ def Specialite(cis: str) -> Component:
     )
 
 
-def Header(cis: str) -> Component:
-    df_spe = specialite.get_specialite_df(cis)
+def Header(df_spe: pd.DataFrame) -> Component:
     series_spe = fetch_data.as_series(df_spe)
 
     return html.Div(
@@ -141,8 +141,7 @@ def Header(cis: str) -> Component:
     )
 
 
-def SubstanceLinks(cis: str) -> Component:
-    df_sub = specialite.list_substances(cis)
+def SubstanceLinks(df_sub: pd.DataFrame) -> Component:
     return html.Div(
         [
             html.A(
@@ -156,19 +155,17 @@ def SubstanceLinks(cis: str) -> Component:
     )
 
 
-def Description(cis: str) -> Component:
-    df_spe = specialite.get_specialite_df(cis)
+def Description(df_spe: pd.DataFrame, df_desc: pd.DataFrame, df_atc: pd.DataFrame, df_sub: pd.DataFrame) -> Component:
     series_spe = fetch_data.as_series(df_spe)
-    df_desc = specialite.get_description_df(cis)
     series_desc = fetch_data.as_series(df_desc)
-    df_atc = specialite.get_atc_df(cis)
     series_atc = fetch_data.as_series(df_atc)
+    cis = series_spe.name
 
     return TopicSection(
         Box(
             [
                 html.Article(
-                    [ArticleTitle("Substance(s) active(s)"), SubstanceLinks(cis)]
+                    [ArticleTitle("Substance(s) active(s)"), SubstanceLinks(df_sub)]
                 ),
                 html.Article(
                     [
@@ -227,7 +224,7 @@ def NoData() -> html.Div:
     return html.Div(
         [
             html.Img(
-                src="/assets/illu_no_data.svg",
+                src=app.get_sasset_url("illu_no_data.svg"),
                 className="img-fluid",
                 alt="Responsive image",
             ),
@@ -265,18 +262,22 @@ def StackBarGraph(df: pd.DataFrame, field: str) -> Graph:
         )
 
 
-def ErreursMedicamenteuses(cis: str) -> Component:
-    df_ei = specialite.get_erreur_med_effet_indesirable(cis)
-    ei_figures = [
+def BoxPourcentageEffetsIndesirable(df_ei: pd.DataFrame) -> Component: 
+    if df_ei is None: 
+        return NoData()
+
+    return FigureGraph([
         {
             "figure": "{}%".format(round(series.pourcentage, 2)),
             "caption": EI[series.effet_indesirable],
             "img": EI_IMG_URL[series.effet_indesirable],
         }
         for cis, series in df_ei.iterrows()
-    ]
+    ])
 
-    df_pop = specialite.get_erreur_med_population(cis)
+def BoxRepartitionPopulationConcernee(df_pop: pd.DataFrame) -> Component:
+    if df_pop is None:
+        return NoData()
     fig_pop = go.Figure(
         go.Pie(
             labels=df_pop.population_erreur,
@@ -284,12 +285,40 @@ def ErreursMedicamenteuses(cis: str) -> Component:
             marker_colors=PIE_COLORS_SPECIALITE,
         )
     ).update_layout(PIE_LAYOUT)
+    return Graph(figure=fig_pop, responsive=True,)
 
-    df_cause = specialite.get_erreur_med_cause(cis)
-    df_nat = specialite.get_erreur_med_nature(cis)
-    df_denom = specialite.get_erreur_med_denom(cis)
-
+def BoxListDenomination(df_denom): 
+    if df_denom is None:
+        return NoData()
     df_denom.denomination = df_denom.denomination.str.capitalize()
+    return dash_table.DataTable(
+        id="denomination-table",
+        columns=[
+            {"name": i, "id": i}
+            for i in df_denom[
+                ["denomination"]
+            ].columns
+        ],
+        data=df_denom.to_dict("records"),
+        page_size=10,
+        style_as_list_view=True,
+        style_table={"overflowX": "auto"},
+        style_cell={
+            "height": "50px",
+            "backgroundColor": "#FAFAFA",
+        },
+        style_data={
+            "fontSize": "14px",
+            "fontWeight": "400",
+            "font-family": "Roboto",
+            "lineHeight": "18px",
+            "textAlign": "left",
+        },
+        style_header={"display": "none"},
+    )
+
+
+def ErreursMedicamenteuses(df_ei: pd.DataFrame, df_pop: pd.DataFrame, df_cause: pd.DataFrame, df_nat: pd.DataFrame, df_denom: pd.DataFrame) -> Component:
 
     return TopicSection(
         [
@@ -308,17 +337,12 @@ def ErreursMedicamenteuses(cis: str) -> Component:
                 [
                     GraphBox(
                         "Existence d’effets indésirables",
-                        [FigureGraph(ei_figures)],
+                        [BoxPourcentageEffetsIndesirable(df_ei)],
                         class_name_wrapper="col-md-6",
                     ),
                     GraphBox(
                         "Répartition de la population concernée",
-                        [
-                            Graph(
-                                figure=fig_pop,
-                                responsive=True,
-                            )
-                        ],
+                        [BoxRepartitionPopulationConcernee(df_pop)],
                         class_name_wrapper="col-md-6",
                     ),
                 ]
@@ -346,31 +370,7 @@ def ErreursMedicamenteuses(cis: str) -> Component:
                     GraphBox(
                         "Liste des dénominations des médicaments concernés par ces erreurs médicamenteuses",
                         [
-                            dash_table.DataTable(
-                                id="denomination-table",
-                                columns=[
-                                    {"name": i, "id": i}
-                                    for i in df_denom[
-                                        ["denomination"]
-                                    ].columns
-                                ],
-                                data=df_denom.to_dict("records"),
-                                page_size=10,
-                                style_as_list_view=True,
-                                style_table={"overflowX": "auto"},
-                                style_cell={
-                                    "height": "50px",
-                                    "backgroundColor": "#FAFAFA",
-                                },
-                                style_data={
-                                    "fontSize": "14px",
-                                    "fontWeight": "400",
-                                    "font-family": "Roboto",
-                                    "lineHeight": "18px",
-                                    "textAlign": "left",
-                                },
-                                style_header={"display": "none"},
-                            )
+                            BoxListDenomination(df_denom)
                         ],
                         class_name_wrapper="col-md-12",
                     ),
@@ -381,8 +381,7 @@ def ErreursMedicamenteuses(cis: str) -> Component:
     )
 
 
-def EffetsIndesirables(cis: str) -> Component:
-    df_sub = specialite.list_substances(cis)
+def EffetsIndesirables(df_sub: pd.DataFrame) -> Component:
 
     return TopicSection(
         [
