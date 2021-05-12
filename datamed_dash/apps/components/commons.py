106953eb4f738995b8788a7ada79a1b1eff60cad
jmunozz <jmunoz@student.42.fr>
2021-05-12 16:09:55 +0200

    FIX: layout for rupture page

--- datamed_dash/apps/components/commons.py
+++ datamed_dash/apps/components/commons.py
@@ -66,11 +66,7 @@ def get_sexe_figures_from_df(df: pd.DataFrame, column: str) -> List[Dict]:
 
 def makePie(labels: pd.Series, values: pd.Series, pie_colors: List):
     return go.Figure(
-        go.Pie(
-            labels=labels,
-            values=values,
-            marker_colors=pie_colors,
-        )
+        go.Pie(labels=labels, values=values, marker_colors=pie_colors,)
     ).update_layout(PIE_LAYOUT)
 
 
@@ -115,10 +111,7 @@ def Tooltip() -> Component:
                     ),
                     html.Div(
                         [
-                            html.Span(
-                                "Attention : ",
-                                className="normal-text-bold",
-                            ),
+                            html.Span("Attention : ", className="normal-text-bold",),
                             html.Span(
                                 "Un patient est comptabilisé autant de fois qu’il a acheté de boîtes "
                                 "(ou présentations) différentes de la spécialité / substance active. Pour "
@@ -162,13 +155,7 @@ def Utilisation(df_expo: Optional[pd.DataFrame]) -> Component:
 
     df = pd.DataFrame(
         {
-            "Utilisation": [
-                "Très faible",
-                "Faible",
-                "Modéré",
-                "Élevé",
-                "Très élevé",
-            ],
+            "Utilisation": ["Très faible", "Faible", "Modéré", "Élevé", "Très élevé",],
             "Nombre de patients (niveau spécialité)": [
                 "< 1 000",
                 "1 000 - 5 000",
@@ -294,11 +281,13 @@ def PatientsTraites(
 
 def Header(series_spe: pd.Series, type="specialite") -> Component:
     if type == "substance":
+        title = series_spe.nom.capitalize()
         css_class = "Header-isSubstance"
         icon_url = app.get_asset_url("substance_icon.svg")
         type_label = "Substance active"
         help_link = html.A("Qu'est-ce qu'une substance active ?", id="definition-open")
-    else:
+    elif type == "specialite":
+        title = series_spe.nom.capitalize()
         css_class = "Header-isSpecialite"
         df_icones = specialite.get_icones(series_spe.name)
         series_icones = fetch_data.as_series(df_icones)
@@ -309,17 +298,20 @@ def Header(series_spe: pd.Series, type="specialite") -> Component:
         help_link = html.A(
             "Qu'est-ce qu'une spécialité de médicament ?", id="definition-open"
         )
+    elif type == "rupture":
+        title = "Observatoire des ruptures de stock"
+        css_class = "Header-isRupture"
+        icon_url = app.get_asset_url("Liquide-64.png")
+        type_label = "Base de données"
+        help_link = html.A("Qu'est-ce qu'une base de donnée?", id="definition-open")
 
     return html.Div(
         html.Div(
             [
-                html.Div(
-                    html.Img(src=icon_url),
-                    className="content-header-img",
-                ),
+                html.Div(html.Img(src=icon_url), className="content-header-img",),
                 html.Div(
                     [
-                        html.Div(series_spe.nom.capitalize(), className="heading-4"),
+                        html.Div(title, className="heading-4"),
                         html.Div(type_label, className="large-text"),
                         help_link,
                         dbc.Modal(
