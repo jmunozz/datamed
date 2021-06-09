6ad506ef61603ffe9018092f950a2ffa1a9c889a
jmunozz <jmunoz@student.42.fr>
2021-06-09 11:20:14 +0200

    ADD: scrapping for publications + requirements and mod DBURL

--- create_database/bdpm_scrapper.py
+++ create_database/bdpm_scrapper.py
@@ -1,10 +1,12 @@
 import json
-from typing import List, Dict
+from typing import List, Dict, Tuple
+import functools
 
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+from multiprocessing import Pool
 
 import db
 import settings
@@ -14,49 +16,88 @@ engine = db.connect_db()
 
 def get_cis_list() -> List:
     df_cis = pd.read_sql("specialite", engine)
-    return df_cis.cis.unique()
+    return list(df_cis.cis.unique())
 
 
-def scrap_bdpm(cis_list: List) -> List[Dict]:
-    descriptions = []
-    for cis in tqdm(cis_list):
-        page = requests.get(
-            "http://base-donnees-publique.medicaments.gouv.fr/extrait.php?specid={}".format(
-                cis
-            )
+def check_publication_type(_type: str):
+    if _type.lower() not in ALLOWED_TYPES:
+        return "Autre"
+    return _type
+
+
+@functools.lru_cache(maxsize=None)
+def scrap_bdpm(cis: str) -> Dict:
+    print(f"scrap_bdpm for cis: ${cis}")
+    page = requests.get(
+        "http://base-donnees-publique.medicaments.gouv.fr/extrait.php?specid={}".format(
+            cis
         )
+    )
+    # Fetch webpage
+    soup = BeautifulSoup(page.content, "html.parser")
+    # print(soup.prettify())
+    # Find description elements
+    description_content = ""
+    description_elements = soup.find_all("p", {"class": "AmmCorpsTexte"})
+    if description_elements:
+        for ele in description_elements:
+            if (
+                ele.text.replace("\n", " ")
+                .strip()
+                .startswith("Classe pharmacothérapeutique")
+                or "ATC" in ele.text.replace("\n", " ").strip()
+            ):
+                continue
+            else:
+                description_content += (
+                    ele.text.replace("\n", " ").strip().lower().capitalize() + " "
+                )
+    description = dict(cis=cis, description=description_content)
+    # Find publications elements
+    publications = []
+    publications_ul_elem = soup.select("#liste_infos_importantes > li > a")
+    if publications_ul_elem:
+        for p in publications_ul_elem:
+            text = str(p.string)
+            type = check_publication_type(text.split("-")[-1].strip())
+            if type == "Autre":
+                title = text
+            else:
+                title = "-".join(text.split("-")[:-1])
+            publications.append(dict(link=p["href"], type=type, title=title, cis=cis))
 
-        # Fetch webpage
-        soup = BeautifulSoup(page.content, "html.parser")
-        # print(soup.prettify())
-
-        description_elements = soup.find_all("p", {"class": "AmmCorpsTexte"})
-
-        if description_elements:
-            description = ""
-            for ele in description_elements:
-                if (
-                    ele.text.replace("\n", " ")
-                    .strip()
-                    .startswith("Classe pharmacothérapeutique")
-                    or "ATC" in ele.text.replace("\n", " ").strip()
-                ):
-                    continue
-                else:
-                    description += (
-                        ele.text.replace("\n", " ").strip().lower().capitalize() + " "
-                    )
-            descriptions.append({"cis": cis, "description": description})
-    return descriptions
+    return (description, publications)
 
 
 def create_description_table():
     cis_list = get_cis_list()
-    descriptions = scrap_bdpm(cis_list)
+    with Pool(10) as p:
+        all_scraps = p.map(scrap_bdpm, cis_list)
+        all_descriptions = list(map(lambda x: x[0], all_scraps))
+        with open("data/descriptions.json", "w") as outfile:
+            json.dump(all_descriptions, outfile)
+        df = pd.DataFrame(all_descriptions, columns=["cis", "description"])
+        df = df.set_index("cis")
+        db.create_table_from_df(df, settings.files["description"]["to_sql"])
 
-    with open("data/descriptions.json", "w") as outfile:
-        json.dump(descriptions, outfile)
 
-    df = pd.DataFrame(descriptions, columns=["cis", "description"])
-    df = df.set_index("cis")
-    db.create_table_from_df(df, settings.files["description"]["to_sql"])
+ALLOWED_TYPES = ["point d'information", "communiqué"]
+
+
+def create_publications_table():
+    cis_list = get_cis_list()
+    with Pool(10) as p:
+        flatten = lambda t: [item for sublist in t for item in sublist]
+        all_scraps = p.map(scrap_bdpm, cis_list)
+        all_publications = flatten(list(map(lambda x: x[1], all_scraps)))
+        with open("data/publications.json", "w") as outfile:
+            json.dump(all_publications, outfile)
+        df = pd.DataFrame(all_publications, columns=["cis", "title", "type", "link"])
+        df = df.set_index("cis")
+        db.create_table_from_df(df, settings.files["publications"]["to_sql"])
+
+
+if __name__ == "__main__":
+    create_description_table()
+    create_publications_table()
+
