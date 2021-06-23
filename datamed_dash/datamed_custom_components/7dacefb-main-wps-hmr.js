c755765653159e295999df22de1e0b891746b895
jmunozz <jmunoz@student.42.fr>
2021-06-23 14:40:25 +0200

    ADD: navbar is ok

---
+++ datamed_dash/datamed_custom_components/7dacefb-main-wps-hmr.js
@@ -0,0 +1,15 @@
+webpackHotUpdatedatamed_custom_components("main",{
+
+/***/ "./src/lib/components/NavBar.scss":
+/*!****************************************!*\
+  !*** ./src/lib/components/NavBar.scss ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module parse failed: Unexpected token (1:0)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n> .CusotmNavBar {\n|     position: fixed;\n|     background-color: white;");
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiI3ZGFjZWZiLW1haW4td3BzLWhtci5qcyIsInNvdXJjZVJvb3QiOiIifQ==
--- datamed_dash/datamed_custom_components/_imports_.py
+++ datamed_dash/datamed_custom_components/_imports_.py
@@ -1,9 +1,11 @@
 from .Accordion import Accordion
+from .NavBar import NavBar
 from .SearchBar import SearchBar
 from .Treemap import Treemap
 
 __all__ = [
     "Accordion",
+    "NavBar",
     "SearchBar",
     "Treemap"
 ]
