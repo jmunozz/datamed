04d908364f54fa26b1a90844168d5e037ca5bffa
Line Rahal <48203532+lrahal@users.noreply.github.com>
2021-06-30 12:07:22 +0200

    Clean repo

--- datamed_dash/apps/app1.py
+++ datamed_dash/apps/app1.py
@@ -1,14 +1,9 @@
+from apps.components.commons import SideEffects
 from dash_html_components import Div
 
-from .components.arrow import Arrow
-from .components.donnees_une import DonneesUne
 from .components.footer import Footer
-
-from .components.navbar import Navbar
-from .components.plateforme import Plateforme
 from .components.frontpage import FrontPage
-from apps.components.commons import SideEffects
-
+from .components.navbar import Navbar
 
 layout = Div(
     [Navbar(), *FrontPage(), Footer(), SideEffects()],
