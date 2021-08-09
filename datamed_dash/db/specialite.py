d80da1c9e5114cd556af498e94397791fbb53a3d
Line Rahal <48203532+lrahal@users.noreply.github.com>
2021-08-09 14:11:39 +0200

    Add typing

--- datamed_dash/db/specialite.py
+++ datamed_dash/db/specialite.py
@@ -8,7 +8,7 @@ from .fetch_data import fetch_table, return_sub_df_or_none
 
 # cis can be a str or a list
 @cache.memoize(300)
-def get_specialite_df(cis):
+def get_specialite_df(cis: str):
     return return_sub_df_or_none(fetch_table("specialite", "cis"), cis)
 
 
@@ -20,67 +20,67 @@ def list_specialite():
     return fetch_table("specialite", "cis")
 
 
-def get_specialite_substance_df(cis):
+def get_specialite_substance_df(cis: str):
     return return_sub_df_or_none(fetch_table("specialite_substance", "cis"), cis)
 
 
-def get_sexe_df(cis):
+def get_sexe_df(cis: str):
     return return_sub_df_or_none(
         fetch_table("specialite_patient_sexe_ordei", "cis"), cis
     )
 
 
-def get_erreur_med_effet_indesirable(cis):
+def get_erreur_med_effet_indesirable(cis: str):
     return return_sub_df_or_none(
         fetch_table("erreur_med_effet_indesirable", "cis"), cis
     )
 
 
-def get_erreur_med_population(cis):
+def get_erreur_med_population(cis: str):
     return return_sub_df_or_none(fetch_table("erreur_med_population", "cis"), cis)
 
 
-def get_erreur_med_cause(cis):
+def get_erreur_med_cause(cis: str):
     return return_sub_df_or_none(fetch_table("erreur_med_cause", "cis"), cis)
 
 
-def get_erreur_med_nature(cis):
+def get_erreur_med_nature(cis: str):
     return return_sub_df_or_none(fetch_table("erreur_med_nature", "cis"), cis)
 
 
-def get_erreur_med_denom(cis):
+def get_erreur_med_denom(cis: str):
     return return_sub_df_or_none(fetch_table("erreur_med_cis_denomination", "cis"), cis)
 
 
-def get_exposition(cis):
+def get_exposition(cis: str):
     return return_sub_df_or_none(fetch_table("specialite_exposition", "cis"), cis)
 
 
-def get_age_df(cis):
+def get_age_df(cis: str):
     return return_sub_df_or_none(
         fetch_table("specialite_patient_age_ordei", "cis"), cis
     )
 
 
-def get_description_df(cis):
+def get_description_df(cis: str):
     return return_sub_df_or_none(fetch_table("description", "cis"), cis)
 
 
-def get_atc_df(cis) -> pd.DataFrame:
+def get_atc_df(cis: str) -> pd.DataFrame:
     spe_atc_df = return_sub_df_or_none(fetch_table("specialite_atc", "cis"), cis)
     if spe_atc_df is None:
         return None
     return pd.merge(spe_atc_df, list_atc(), left_on="atc", right_index=True, how="left")
 
 
-def list_substances(cis):
+def list_substances(cis: str):
     from .substance import get_substance_df
 
     df_spe_sub = get_specialite_substance_df(cis)
     return get_substance_df(df_spe_sub["code_substance"].values)
 
 
-def get_presentation_df(cis):
+def get_presentation_df(cis: str):
     return return_sub_df_or_none(fetch_table("presentation", "cis"), cis)
 
 
@@ -101,11 +101,11 @@ def get_ruptures(cis: str, df_spe: pd.DataFrame):
     return df_ruptures[df_ruptures.nom.apply(lambda x: nom in x if x else None)]
 
 
-def get_icones(cis):
+def get_icones(cis: str):
     return return_sub_df_or_none(fetch_table("icones", "cis"), cis)
 
 
-def get_erreur_med_init(cis):
+def get_erreur_med_init(cis: str):
     return return_sub_df_or_none(fetch_table("erreur_med_initial", "cis"), cis)
 
 
@@ -113,6 +113,6 @@ def get_erreur_med_gravite(cis):
     return return_sub_df_or_none(fetch_table("erreur_med_gravite", "cis"), cis)
 
 
-def get_publications(cis):
+def get_publications(cis: str):
     df = return_sub_df_or_none(fetch_table("publications", "cis"), cis)
     return df[::-1]
